c1eaebe3d793fc2e0cae419f517bf2b11f63368d
Ruth Kitchin Tillman <ruthtillman@gmail.com>
2018-07-20 15:10:42 -0400

    Added functional logging to uploading new subjects
    Brought in the ASnake logging and made that work to my satisfaction.

--- upload-new-subjects.py
+++ upload-new-subjects.py
@@ -1,6 +1,15 @@
 # Import the various client libraries needed to make this work. And some friends for now.
-
 import os, requests, glob, json, logging, csv, configparser, datetime
+
+# import and set up logging BEFORE touching the client or anything
+import asnake.logging as logging
+
+logname = 'logs/new_subject_upload_' + datetime.datetime.now().strftime('%Y-%m-%d-T-%H-%M') + '.log'
+
+logfile = open(logname, 'w')
+logging.setup_logging(stream=logfile)
+logger = logging.get_logger("upload-new-subjects")
+
 # Bring in the client to work at a very basic level.
 from asnake.client import ASnakeClient
 
@@ -9,44 +18,24 @@ client = ASnakeClient()
 client.authorize()
 
 def upload_json_as_new_subjects(file_dir):
+    logger.info("upload_start", batch_name=batch)
     os.chdir(file_dir)
     subjects = glob.glob("*.json")
-    init_job = name_log()
-    results = []
     for file in subjects:
         subject = json.load(open(file))
         response = client.post('subjects', json=subject).json()
         response['title'] = subject['title']
-        results.append(response)
+        logger.info("upload_subject", response=response)
     os.chdir('..')
-    create_log(init_job, results)
+    logfile.close()
 
-# I swear too much of my time was spent on logging concerns. However part of that is that I just need to be sure I end up with a good log for the next phase of my project.
-
-# This names the log based on when the script starts running and gets a start time.
-# It returns them as a tuple because that's how it is. So it's called to the variable init_job above, then passed to the creation and the values can be accessed that way.
-
-def name_log():
-    logName = datetime.datetime.now().strftime('%Y-%m-%d-T-%H-%M') + "_subject_upload_log.json"
-    jobStart = datetime.datetime.now().isoformat()
-    return (logName, jobStart)
-
-# Makes use of init_log and the dictionary of responses received. Note that this dictionary includes responses which have had titles added to them. That's mostly for my own use.
-
-def create_log(init_job, results):
-    logName = 'logs/' + init_job[0]
-    log = json.loads('{"jobType" : "upload_subjects"}')
-    log['user'] = 'rkt6'
-    log['dateTimeStart'] = init_job[1]
-    log['dateTimeEnd'] = datetime.datetime.now().isoformat()
-    log['responses'] = results
-    with open(logName, "w") as logging:
-        json.dump(log, logging, indent=4)
 
 # This can be made into an input at some point but right now we'll just set to the files directory
 
-subject_location = 'files'
+subject_location = 'first-reserve'
 
 # Call the main function
+
+batch = input("Name your upload batch: ")
 
 upload_json_as_new_subjects(subject_location)
