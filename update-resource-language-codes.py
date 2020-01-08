bb88937c85ee845305d31d87471c89d251718462
Ruth Kitchin Tillman <rkt@ruthtillman.com>
2020-01-08 12:39:06 -0500

    working update

--- update-resource-language-codes.py
+++ update-resource-language-codes.py
@@ -1,15 +1,22 @@
-import json
+import json, os, glob
 
-fileName = "new-5.json"
+def updating_language_codes(sourceDirectory,targetDirectory):
+    os.chdir(sourceDirectory)
+    resources = glob.glob("*.json")
+    for resource in resources:
+        with open(resource, "r") as myJSON:
+              recorddata = json.loads(myJSON.read())
+              if recorddata['finding_aid_language'] == "und" or recorddata['finding_aid_script'] == "Zyyy":
+                  if recorddata['finding_aid_language'] == "und":
+                      recorddata['finding_aid_language'] = "eng"
+                  if recorddata['finding_aid_script'] == "Zyyy":
+                      recorddata['finding_aid_script'] = "Latn"
+                  output = targetDirectory + "/" + resource
+                  with open(output, 'w') as outfile:
+                      json.dump(recorddata, outfile, sort_keys=True, indent=4)
 
-with open(fileName, "r") as myJSON:
-      recorddata = json.loads(myJSON.read())
-      if recorddata['finding_aid_language'] == "und" or recorddata['finding_aid_script'] == "Zyyy":
-          if recorddata['finding_aid_language'] == "und":
-              recorddata['finding_aid_language'] = "eng"
-          if recorddata['finding_aid_script'] == "Zyyy":
-              recorddata['finding_aid_script'] = "Latn"
-          with open("test.json", 'w') as outfile:
-              json.dump(recorddata, outfile, sort_keys=True, indent=4)
-      else:
-          print("nope")
+
+sourceDirectory=input("Path to the directory containing resources: ")
+tarDirectory=input("Path to the output directory: ")
+
+updating_language_codes(sourceDirectory,tarDirectory)
