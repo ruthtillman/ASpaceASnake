e4a6d6cd7599c120d14f038fb9f14c5398594edc
Ruth Kitchin Tillman <rkt@ruthtillman.com>
2020-01-08 12:15:16 -0500

    manual language and script code updates

--- update-resource-language-codes.py
+++ update-resource-language-codes.py
@@ -1,1 +1,15 @@
 import json
+
+fileName = "new-5.json"
+
+with open(fileName, "r") as myJSON:
+      recorddata = json.loads(myJSON.read())
+      if recorddata['finding_aid_language'] == "und" or recorddata['finding_aid_script'] == "Zyyy":
+          if recorddata['finding_aid_language'] == "und":
+              recorddata['finding_aid_language'] = "eng"
+          if recorddata['finding_aid_script'] == "Zyyy":
+              recorddata['finding_aid_script'] = "Latn"
+          with open("test.json", 'w') as outfile:
+              json.dump(recorddata, outfile, sort_keys=True, indent=4)
+      else:
+          print("nope")
