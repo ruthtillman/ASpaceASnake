d0906afef3e3e1f95a7b50d89b76af5e7a59dc92
Ruth Kitchin Tillman <rkt@ruthtillman.com>
2020-01-08 16:16:58 -0500

    just changes resource name

--- get-access-restrict-notes.py
+++ get-access-restrict-notes.py
@@ -1,4 +1,4 @@
-import os, json, re, csv
+import os, json, re, csv, glob
 
 # assumes we are iterating over a full download of resources... which I just happen to have right now.
 
@@ -6,12 +6,11 @@ def get_restrictions(writer):
     files = glob.glob("*.json")
     for jsonFile in files:
           res_num = jsonFile.rstrip('.json')
-          subject = json.load(open(jsonFile))
-          for note in subject['notes']:
+          resource = json.load(open(jsonFile))
+          for note in resource['notes']:
             if 'type' in note and note['type'] == 'userestrict':
               for sub in note['subnotes']:
                 writer.writerow({'ASpace_ID' : res_num, 'Note_ID' : note['persistent_id'], 'Note_Type' : note['type'], 'Note_Content': sub['content'] })
-
 
 def write_notes_csv(csvName):
     fieldnames = ['ASpace_ID', 'Note_ID', 'Note_Type', 'Note_Content']
