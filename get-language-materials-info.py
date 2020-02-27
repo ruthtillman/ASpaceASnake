5b57f4cf27ae9c0167d7791c286ce07481975138
Ruth Kitchin Tillman <rkt@ruthtillman.com>
2020-02-27 09:34:50 -0500

    small updates

--- get-language-materials-info.py
+++ get-language-materials-info.py
@@ -2,28 +2,33 @@ import os, json, re, csv, glob
 
 # assumes we are iterating over a full download of resources... which I just happen to have right now.
 
-def get_restrictions(writer):
+def get_language(source_dir,tar_dir):
+    os.chdir(source_dir)
     files = glob.glob("*.json")
     for jsonFile in files:
           res_num = jsonFile.rstrip('.json')
           resource = json.load(open(jsonFile))
-          for language in resource['language_and_script']:
+          for language in resource['lang_materials']:
               if 'language_and_script' in language:
-                language['language_and_script']['language']
-              else:
-                language
+                langcode = language['language_and_script']['language']
+                if langcode == 'und':
+                    language['language_and_script']['language'] = 'eng'
+                    filepath = tar_dir + "/" + jsonFile
+                    with open(filepath, 'w') as outfile:
+                        json.dump(resource, outfile, sort_keys=True, indent=4)
 
-          for note in resource['notes']:
-            if 'type' in note and note['type'] == 'userestrict':
-              for sub in note['subnotes']:
-                writer.writerow({'ASpace_ID' : res_num, 'Note_ID' : note['persistent_id'], 'Note_Type' : note['type'], 'Note_Content': sub['content'] })
+## get all the ones without language keys?
+
+def no_lang_key(source_dir):
+    files = glob.glob("*.json")
+    for jsonFile in files:
+      res_num = jsonFile.rstrip('.json')
+      subject = json.load(open(jsonFile))
+      for language in subject["lang_materials"]:
+        if "language_and_script" not in language:
+          language
 
 
-def write_notes_csv(csvName):
-    fieldnames = ['ASpace_ID', 'Note_ID', 'Note_Type', 'Note_Content']
-    with open(csvName, 'w', newline='') as outputFile:
-        writer = csv.DictWriter(outputFile, fieldnames=fieldnames)
-        writer.writeheader()
-        get_restrictions(writer)
-
-write_notes_csv('../userestrict_notes.csv')
+source_dir = input("Name of soure directory: ")
+tar_dir = input("Relative path from source to output directory (no trailing slash): ")
+get_language(source_dir,tar_dir)
